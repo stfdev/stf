fe6a40ad54e5441350da679f6a321e7a6d6139eb
Marvin Herrera <marvinahv@gmail.com>
2012-03-12 17:58:26 -0600

    Fixed buttons and simplified the site to receive tasks and give basic information about the service

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.1.3'
 
 gem 'pg'
 gem 'haml-rails'
+gem 'maruku'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Stf::Application.routes.draw do
   root :to => 'home#index'
   resources :users, :tasks
+  
+  get '/about' => 'home#about'
+  get '/apply' => 'home#apply'
 end
